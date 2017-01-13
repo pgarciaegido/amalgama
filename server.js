07b71c1eafc5165b9c02107a3f857bef2a1d561c
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-01-13 18:27:02 +0100

    setting heroku database

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var config = require('./config')
 var mongoose = require('mongoose')
 var app = require('./app')
 
-// mongoose.connect('mongodb://heroku_b309pz7s:rokbgcdt1m9r767tmhugv141vi@ds161008.mlab.com:61008/heroku_b309pz7s') // localhost/nombrebasedatos
-mongoose.connect(config.db)
+mongoose.connect('mongodb://heroku_b309pz7s:rokbgcdt1m9r767tmhugv141vi@ds161008.mlab.com:61008/heroku_b309pz7s') // localhost/nombrebasedatos
+// mongoose.connect(config.db)
 
 app.listen(config.port, function () {
+  console.log(process.env)
+  console.log(process.env.MONGODB)
   console.log('Escuchando en puerto ' + config.port + '!')
 })
