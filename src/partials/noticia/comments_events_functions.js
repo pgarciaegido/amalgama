438f12485ca49694dbfcfd01c80c01f5d2ad92cd
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-02-17 14:22:29 +0100

    Styles for comment button refined

--- src/partials/noticia/comments_events_functions.js
+++ src/partials/noticia/comments_events_functions.js
@@ -17,17 +17,24 @@ module.exports = {
 // //////////////////////////// Functions
 
 // ---- Displays textarea
+// Adds class to the div container of the whole structure, to it displays nicely
+// then removes the overflow on the div containing the list of comments
+// removes que Id of the button, so we can add it on an event listener close
 function createShow (create, comment, button) {
   create.addClass('create-comment-opened')
   comment.scrollTop(0).css('overflow-y', 'hidden')
   button.removeAttr('id')
 }
 
-// Opens input in agree
+// --- Opens input in agree
+// Requires the DOM elements
+// Calls createShow
+// Gives the 'open' comment button an id so now is used to closeit
 function commentAgree () {
   const v = require('./comments_events_vars')
   createShow(v.createAgree, v.commentsAgree, v.comentarAgree)
   v.comentarAgree.attr('id', 'comentar-closeagree')
+  v.comentarAgree.html('Cerrar')
 }
 
 // Opens input in disagree
@@ -35,12 +42,15 @@ function commentDisagree () {
   const v = require('./comments_events_vars')
   createShow(v.createDisagree, v.commentsDisagree, v.comentarDisagree)
   v.comentarDisagree.attr('id', 'comentar-closedisagree')
+  v.comentarDisagree.html('Cerrar')
 }
 
 // ---- Hide textarea
+// The logics are the same than for opening but the other way around
 function createHide (create, comment, textarea, button) {
   create.removeClass('create-comment-opened')
   comment.css('overflow-y', 'scroll')
+  // empties the textarea
   textarea.val('')
   button.removeAttr('id')
 }
@@ -49,28 +59,31 @@ function commentCloseAgree () {
   const v = require('./comments_events_vars')
   createHide(v.createAgree, v.commentsAgree, v.textAgree, v.comentarAgree)
   v.comentarAgree.attr('id', 'comentar-agree')
+  v.comentarAgree.html('Comentar')
 }
 
 function commentCloseDisagree () {
   const v = require('./comments_events_vars')
   createHide(v.createDisagree, v.commentsDisagree, v.textDisagree, v.comentarDisagree)
   v.comentarDisagree.attr('id', 'comentar-disagree')
+  v.comentarDisagree.html('Comentar')
+
 }
 
 
 
-// --------- Send comments
-function enviarComments () {
-  const v = require('./comments_events_vars')
-  if ($('#cancelar-comments').data().resolve === 'agree') {
-    func.addComment(v.textAgree, v.commentsAgree)
-    func.createHide(v.createAgree, v.commentsAgree, v.textAgree)
-  } else if ($('#cancelar-comments').data().resolve === 'disagree') {
-    func.addComment(v.textDisagree, v.commentsDisagree)
-    func.createHide(v.createDisagree, v.commentsDisagree, v.textDisagree)
-  }
-  $('#cancelar-comments').data().resolve = undefined
-}
+// // --------- Send comments
+// function enviarComments () {
+//   const v = require('./comments_events_vars')
+//   if ($('#cancelar-comments').data().resolve === 'agree') {
+//     func.addComment(v.textAgree, v.commentsAgree)
+//     func.createHide(v.createAgree, v.commentsAgree, v.textAgree)
+//   } else if ($('#cancelar-comments').data().resolve === 'disagree') {
+//     func.addComment(v.textDisagree, v.commentsDisagree)
+//     func.createHide(v.createDisagree, v.commentsDisagree, v.textDisagree)
+//   }
+//   $('#cancelar-comments').data().resolve = undefined
+// }
 
 
 // ------- Likes / unlikes comment
