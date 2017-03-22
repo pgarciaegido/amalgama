42e4bb05a55d94cc3765948ab411f3c9516bbf79
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-22 14:27:37 +0100

    Setting up production mode

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/users'
+  dbDev: 'mongodb://localhost:27017/users',
+  dbProd: 'mongodb://heroku_b309pz7s:rokbgcdt1m9r767tmhugv141vi@ds161008.mlab.com:61008/heroku_b309pz7s'
 }
--- server.js
+++ server.js
@@ -2,8 +2,16 @@ var config = require('./config')
 var mongoose = require('mongoose')
 var app = require('./app')
 
-// mongoose.connect('mongodb://heroku_b309pz7s:rokbgcdt1m9r767tmhugv141vi@ds161008.mlab.com:61008/heroku_b309pz7s') // localhost/nombrebasedatos
-mongoose.connect(config.db)
+// Checks if is working on development or production
+if (app.settings.env === 'development'){
+  mongoose.connect(config.dbDev)
+}
+else {
+  mongoose.connect(config.dbProd)
+}
+
+
+console.log('app.settings.env -_> ' + app.settings.env)
 
 app.listen(config.port, function () {
   console.log('Escuchando en puerto ' + config.port + '!')
