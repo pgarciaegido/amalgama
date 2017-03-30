3269d44c18bdd9810dc2e8022a51a6fcd07e5290
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-30 13:36:07 +0200

    Fixing /app backend

--- routes/app.js
+++ routes/app.js
@@ -2,11 +2,14 @@ var express = require('express')
 var router = express.Router()
 var render = require('../controllers/render').renderIndex
 
-router.get('/', render)
+router.get('/', function(req, res) {
+  if (req.session) render()
+  res.redirect('/invitado')
+})
 
 router.get('/logout', function (req, res) {
   req.session = null
-  res.redirect('/app')
+  res.redirect('/invitado')
 })
 
 router.get('/noticia/:id', render)
